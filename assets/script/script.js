f5701257af27d40ef003f8b62440ab32dd7c9fc8
Gefei Han <gefei.han@berkeley.edu>
2017-11-19 23:16:11 -0800

    DONE

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,21 @@
+
 $(document).ready(function() {
 
+$('#grid').hide().fadeIn(800, function() {});
+$('#intro').hide().fadeIn(800, function() {});
+$('#socialmedias').hide().fadeIn(800, function() {});
 
 
 
-
+$("#name").hover(
+   function(){
+      // hover code
+      $(this).html("<span style = 'color:black;'>Gefei <i>(Ge·<span style = 'color:#00B23B;'>Faye</span>)</i></span>");
+   }, function(){
+      // unhover code 
+      $(this).html("Faye");
+   }
+);
 
 
 $(".grid-item").hover(
@@ -19,3 +31,4 @@ $(".grid-item").hover(
   
 
 });
+
