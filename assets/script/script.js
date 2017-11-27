b415f9d28a993706c883e90b915eb7553b1f1791
Gefei Han <gefei.han@berkeley.edu>
2017-11-27 13:33:07 -0800

    jQuery Comments added. Sorry I finished the project early and didn’t see the project update until a few mins ago - Hope this is okay!

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,19 @@
 
 $(document).ready(function() {
 
+
+// jQuery Function Number 1
 $('#grid').hide().fadeIn(800, function() {});
+// jQuery Function Number 2
 $('#intro').hide().fadeIn(800, function() {});
 $('#socialmedias').hide().fadeIn(800, function() {});
 
 
-
+// jQuery Function Number 3
 $("#name").hover(
    function(){
       // hover code
+      // jQuery Function Number 4
       $(this).html("<span style = 'color:black;'>Gefei <i>(Ge·<span style = 'color:#00B23B;'>Faye</span>)</i></span>");
    }, function(){
       // unhover code 
@@ -21,9 +25,11 @@ $("#name").hover(
 $(".grid-item").hover(
    function(){
       // hover code
+      // jQuery Function Number 5
       $(this).children().first().css("opacity","1");
    }, function(){
       // unhover code 
+      // jQuery Function Number 6
       $(this).children().first().css("opacity","0");
    }
 );
